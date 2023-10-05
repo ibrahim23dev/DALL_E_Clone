c29928871a527d4a549be492e4265f6e7bb3ee80
Mohammad Ibrahim <ibrahim.soft.dev23@gmail.com>
2023-10-05 09:56:49 +0600

    Third
    Changeable

--- client/src/Pages/CreatePost.jsx
+++ client/src/Pages/CreatePost.jsx
@@ -1,10 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+//import { preview } from '../assets'
+import { getRandompromot } from '../utils'
+import { FromField, Loader } from '../components';
+
+function handleSubmit() {
+  
+}
+
+function handleChange(e) {
+  
+}
+function handleisSurpriseMe() {
+  
+}
+
 
 function CreatePost() {
+  const navigate = useNavigate();
+  const [form, setform] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+  const [generatingImage, setgeneratingImage] = useState(false);
+  const [Loader, setLoader] = useState(false);
   return (
-    <div>
-      CreatePost
-    </div>
+    <section className='max-w-7xl max-auto'>
+      <div>
+        <h1 className='text-[32px]'>Create</h1>
+        <p className='mt-4 text-[#666e75] text-[14px] max-w-[500px]'>Here we can Create AI image generator which we can use flexibility</p>
+      </div>
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+
+      </form>
+
+      <div className='flex flex-col gap-5'>
+        <FromField
+          labelName="Your Name"
+          type="text"
+          name="prompt"
+          placeholder="A Dinosaur exploring Cape Town, photography"
+          value={form.name}
+          handleChange={handleChange}
+          isSurpriseMe
+          handleisSurpriseMe={handleisSurpriseMe}
+        />
+      </div>
+      <div className='flex flex-col gap-5 mt-10'>
+        <FromField
+          labelName="prompt"
+          type="text"
+          name="prompt"
+          placeholder="A Dinosaur exploring Cape Town, photography"
+          value={form.name}
+          handleChange={handleChange}
+          isSurpriseMe
+          handleisSurpriseMe={handleisSurpriseMe}
+        />
+      </div>
+    </section>
   )
 }
 
--- client/src/components/FromField.jsx
+++ client/src/components/FromField.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
-function FromField() {
+function FromField({labelName,type,name,placeholder,value,handleChange,isSurpriseMe,handleisSurpriseMe}) {
   return (
     <div>
-      FromField
+      <div className='flex items-center gap-2 mb-3'>
+        <label className='block text-sm font-medium text-gray-900'>
+           {labelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type='button'
+            className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black' onClick={handleisSurpriseMe}>
+              Surprise Me
+          </button>
+        )}
+      </div>
+      <input 
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className='bg-gray-50 border-gray-300 text-sm text-gray-900
+        rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none
+        block w-full p-3 '
+      />
     </div>
   )
 }
